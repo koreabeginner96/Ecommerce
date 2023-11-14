02a939244db2574e5cbb4e9d6014ea06e18dd30d
koreabeginner96 <zx028906@naver.com>
2023-11-14 19:03:08 +0900

    feat : Add project urls & shop urls

--- e_commerce_project/e_commerce_project/urls.py
+++ e_commerce_project/e_commerce_project/urls.py
@@ -1,22 +1,9 @@
-"""
-URL configuration for e_commerce_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# 프로젝트 루트의 urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include 함수를 임포트
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),           # 관리자 페이지
+    path('shop/', include('shop.urls')),       # shop 앱의 URL을 포함
+    # 여기에 다른 앱들의 URL 구성을 추가할 수 있다.
 ]
