2bd9bf4da40286dfbfa96182c4443ec29a26607d
koreabeginner96 <zx028906@naver.com>
2023-11-15 14:09:54 +0900

    feat : Add views.py

--- e_commerce_project/shop/views.py
+++ e_commerce_project/shop/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product  # 모델 임포트
 
-# Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+def product_list(request):
+    products = Product.objects.all()  # 데이터베이스에서 모든 제품을 조회
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)  # 특정 id의 제품을 조회
+    return render(request, 'shop/product_detail.html', {'product': product})
