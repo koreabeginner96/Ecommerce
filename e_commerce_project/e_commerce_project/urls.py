af1ed36f803039ff1c84a11a80353f9f08e31494
koreabeginner96 <zx028906@naver.com>
2023-11-16 13:53:03 +0900

    feat : add login

--- e_commerce_project/e_commerce_project/urls.py
+++ e_commerce_project/e_commerce_project/urls.py
@@ -1,5 +1,6 @@
 # 프로젝트 루트의 urls.py
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include  # include 함수를 임포트
 from shop import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),       # shop 앱의 URL을 포함
     # 여기에 다른 앱들의 URL 구성을 추가할 수 있다.
     path('', views.home, name='home'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+
 ]
