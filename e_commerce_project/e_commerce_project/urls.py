a865f519a85707ee2030f77855fbba5c3492363c
koreabeginner96 <zx028906@naver.com>
2023-11-16 13:31:09 +0900

    feat : add urls.py home

--- e_commerce_project/e_commerce_project/urls.py
+++ e_commerce_project/e_commerce_project/urls.py
@@ -1,9 +1,11 @@
 # 프로젝트 루트의 urls.py
 from django.contrib import admin
 from django.urls import path, include  # include 함수를 임포트
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),           # 관리자 페이지
     path('shop/', include('shop.urls')),       # shop 앱의 URL을 포함
     # 여기에 다른 앱들의 URL 구성을 추가할 수 있다.
+    path('', views.home, name='home'),
 ]
