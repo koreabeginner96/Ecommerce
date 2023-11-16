fa8c24c41f4f15dfd332bb142e79362fe0bbe2ad
koreabeginner96 <zx028906@naver.com>
2023-11-16 17:39:15 +0900

    perf login duplication id api
    id 중복테스트 구현 제거

--- e_commerce_project/shop/forms.py
+++ e_commerce_project/shop/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='유효한 이메일 주소를 입력하세요.')
@@ -9,6 +10,12 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ("username", "email", "password1", "password2")
 
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("이미 존재하는 사용자 이름입니다.")
+        return username
+    
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
