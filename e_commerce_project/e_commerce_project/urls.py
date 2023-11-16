7bbd3d3bb844f65abdd4965d60b8552e4baee647
koreabeginner96 <zx028906@naver.com>
2023-11-16 16:18:03 +0900

    Revert "feat : forgot_password add"
    This reverts commit 9ae89e64fbc9a896d0fcb1968b026aa83b2d906f.

--- e_commerce_project/e_commerce_project/urls.py
+++ e_commerce_project/e_commerce_project/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
-from django.contrib.auth import views as auth_views
 from django.urls import path, include  # include 함수를 임포트
 from shop import views
 from shop.views import SignUpView
@@ -15,8 +14,4 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignUpView.as_view(template_name='registration/signup.html'), name='signup'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
